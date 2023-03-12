c46af57ac6865de3f32b625cae79c589bbd6a0a2
PramodK2808 <pramodkushwaha1328@gmail.com>
2023-03-12 12:43:29 +0530

    code

--- index.js
+++ index.js
@@ -79,58 +79,59 @@ app.post('/register', async(req, res) => {
 
 
 // LOGIN
-
 app.post('/login', async (req, res) => {
     try {
-        const {email, password} = req.body
-        
-    
+        const {email, password} = req.body;
+        // Check if user already exists
         const user = await UserModel.findOne({email})
-        if(!user){
+        if (!user){
             return res.status(404).send({
-                success: false,
-                message:"User Not Registered"
+                success:false,
+                message:"User not registered or Invalid",
+                
+            })
+        }
+        if(!password || !email){
+            return res.status(404).send({
+                success:false,
+                message:"Please Enter email and password",
+
+        })
+    }
+
+        const matchPassword = await compare(password, user.password)
+        if (!matchPassword){
+            return res.status(404).send({
+                success:false,
+                message:"Invalid email or password",
             })
         }
 
-        if(!email || !password) {
-            return res.status(404).send({
-                success: false,
-                message:"Please enter your email and password"
-            })
-        }
-    
-        const matchPass = await compare(password, user.password)
-        if(!matchPass){
-            return res.status(404).send({
-                success: false,
-                message:"Email or password incorrect"
-            })
-    
-        }
-    
-        const token = await JWT.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" })
-    
+        // generate token 
+
+        const token = await JWT.sign({_id:user._id}, process.env.JWT_SECRET, {expiresIn:"7d"})
+
         res.status(200).send({
-            success: true,
-            message: "Login successful",
-            user: {
-                _id: user._id,
-                name: user.name,
-                email: user.email,
-                mobile: user.mobile,
-                isAdmin: user.isAdmin
+            success:true,
+            message:"Login successful",
+            user:{
+                _id:user._id,
+                name:user.name,
+                email:user.email,
+                mobile:user.mobile,
+                isAdmin:user.isAdmin
             },
             token
         })
     } catch (error) {
         res.status(400).send({
-            success: false,
-            message: "Invalid Credentials",
+            success:false,
+            message:"Invalid Credentials",
             error,
         })
     }
 })
+
 
 
 // DEAL OF THE DAY ROUTES
